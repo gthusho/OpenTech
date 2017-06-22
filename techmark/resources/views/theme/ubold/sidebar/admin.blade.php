f61567f5e7056be267cb74aadc55b7d866887c70
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-06-22 10:52:45 -0400

    Modificacion para Sidebar

--- techmark/resources/views/theme/ubold/sidebar/admin.blade.php
+++ techmark/resources/views/theme/ubold/sidebar/admin.blade.php
@@ -21,15 +21,8 @@
                         <li><a href="{{route('admin.rol.index')}}">Rol</a></li>
                     </ul>
                 </li>
-                <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="icon-people"></i><span class="label label-primary pull-right">2</span> <span> Clientes & Proveedores </span>  </a>
-                    <ul class="list-unstyled">
-                        <li><a href="{{route('admin.cliente.index')}}">Clientes</a></li>
-                        <li><a href="{{route('admin.proveedor.index')}}">Proveedores</a></li>
-                    </ul>
-                </li>
-
-
+                @include('theme.ubold.sidebar.liss')
+                @include('theme.ubold.sidebar.diego')
                 {{--
                 <li>
                     <a href="javascript:void(0);" class="waves-effect"><i class="icon-basket"></i><span class="label label-primary pull-right">7</span> <span> Almacen </span>  </a>
