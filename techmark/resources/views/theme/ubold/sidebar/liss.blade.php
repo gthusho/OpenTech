ce3a8ae1a91b16f4994ca3f4811ffaf700a4b62b
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-06-22 10:55:52 -0400

    separacion sidebar

--- techmark/resources/views/theme/ubold/sidebar/liss.blade.php
+++ techmark/resources/views/theme/ubold/sidebar/liss.blade.php
@@ -1,8 +1,9 @@
 
                 <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="icon-people"></i><span class="label label-primary pull-right">2</span> <span> Clientes & Proveedores </span>  </a>
+                    <a href="javascript:void(0); " class="waves-effect"><i class="icon-people"></i><span class="label label-primary pull-right">2</span> <span> Clientes & Proveedores </span>  </a>
                     <ul class="list-unstyled">
                         <li><a href="{{route('admin.cliente.index')}}">Clientes</a></li>
+                        <li><a href="{{route('admin.proveedor.index')}}">Proveedores</a></li>
                         <li><a href="{{route('admin.proveedor.index')}}">Proveedores</a></li>
                     </ul>
                 </li>
