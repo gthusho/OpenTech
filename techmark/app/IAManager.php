0a5339aa3d0b8f5c7266b4987995f3c1fb0ffd17
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-07-05 22:21:41 -0400

    other

--- techmark/app/IAManager.php
+++ techmark/app/IAManager.php
@@ -11,50 +11,7 @@ namespace App;
 
 class IAManager
 {
-    
-   public $items = null;
-   public function __construct($oldCart)
-   {
-       if ($oldCart){
-            $this->items = $oldCart->items;
-       }else{
 
-       }
-   }
-    public function add($item , $id,$cantidad,$costo){
-        $storedItem = ['qty'=>$item->cantidad,'price'=>$item->precio,'item'=>$item];
-        if($this->items){
-            if (array_key_exists($id,$this->items)){
-                $storedItem = $this->items[$id];
-            }
-        }
-        $storedItem['qty'] = $cantidad;
-        $storedItem['price']= $costo;
-        $this->items[$id] = $storedItem;
-    }
-
-    public function getTotalPrice(){
-
-        $tQty = 0;
-        if($this->items!= null ){
-            foreach ($this->items as $item){
-                $tQty +=  $item['price'];
-            }
-        }
-        return $tQty;
-
-    }
-
-    public function getTotalQty(){
-
-        $tQty = 0;
-        if($this->items != null ){
-            foreach ($this->items as $item){
-                $tQty ++;
-            }
-        }
-        return $tQty;
-
-    }
+   
 
 }
