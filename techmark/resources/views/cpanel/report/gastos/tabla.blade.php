9f543111ddb16dfce257ccd5c85f63b958c5ef09
LissC <liseth22794ccl@gmail.com>
2017-07-14 16:10:53 -0400

    gastos

--- techmark/resources/views/cpanel/report/gastos/tabla.blade.php
+++ techmark/resources/views/cpanel/report/gastos/tabla.blade.php
@@ -1,35 +1,26 @@
 
     <table  cellspacing="5"  >
         <tr>
-            <th width="3%" ><strong>#</strong></th>
-            <th width="8%" ><strong>VENTA</strong></th>
-            <th width="8%" ><strong>FECHA</strong></th>
-            <th width="10%"><strong>ARTICULO</strong></th>
-            <th width="12%"><strong>CATEGORIA</strong></th>
-            <th width="10%"><strong>MARCA</strong></th>
-            <th width="10%"><strong>MATERIAL</strong></th>
-            <th width="12%"><strong>CANTIDAD</strong></th>
-            <th width="10%"><strong>UNIDAD</strong></th>
-            <th width="10%"><strong>SUCURSAL</strong></th>
-            <th width="10%"><strong>ALMACEN</strong></th>
+            <th width="5%" ><strong>#</strong></th>
+            <th width="15%"><strong>FECHA</strong></th>
+            <th width="17%" ><strong>USUARIO</strong></th>
+            <th width="20%" ><strong>SUCURSAL</strong></th>
+            <th width="30%"><strong>DESCRIPCION</strong></th>
+            <th width="15%"><strong>MONTO</strong></th>
         </tr>
         <?php
         $i=1;
         ?>
 
-        @foreach($egresos as $row)
+        @foreach($gastos as $row)
             <tr >
                 <td style="border-bottom: 1px dashed black;">{{$i++}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->ventaarticulo->getCode())}}</td>
-                <td style="border-bottom: 1px dashed black;">{{$row->registro}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->articulo->nombre)}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->articulo->categoria->nombre)}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->articulo->marca->nombre)}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->articulo->material->nombre)}}</td>
-                <td style="border-bottom: 1px dashed black;">{{$row->cantidad}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->articulo->medida->nombre)}}</td>
+                <td style="border-bottom: 1px dashed black;">{{date('d/m/Y',strtotime($row->fecha))}}</td>
+                <td style="border-bottom: 1px dashed black;">{{$row->usuario->nombre}}</td>
                 <td style="border-bottom: 1px dashed black;">{{ucwords($row->sucursal->nombre)}}</td>
-                <td style="border-bottom: 1px dashed black;">{{ucwords($row->almacen->nombre)}}</td>
+                <td style="border-bottom: 1px dashed black;">{{ucwords($row->descripcion)}}</td>
+                <td style="border-bottom: 1px dashed black;">{{\App\Tool::convertMoney($row->monto)}}</td>
+
 
             </tr>
         @endforeach
