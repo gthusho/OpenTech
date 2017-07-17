f3ed43254d6e343dbc6202fde97596a3275339eb
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-07-17 10:56:10 -0400

    base de datos

--- techmark/resources/views/cpanel/admin/caja/partials/detalle.blade.php
+++ techmark/resources/views/cpanel/admin/caja/partials/detalle.blade.php
@@ -3,28 +3,24 @@
         <h4 class="panel-title">VENTAS ARTICULOS</h4>
     </div>
     <div class="panel-body">
-        <ul class="list-unstyled">
-            <P></P>
-
-            <table>
+            <table class="table table-hover">
                 <tr>
-                    <td width="50%" ><strong>Ventas de Articulos al Contado</strong></td>
-                    <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                    <td width="50%" ><h5>Ventas de Articulos al Contado</h5></td>
+                    <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
                 </tr>
                 <tr>
-                    <td width="50%" ><strong>Ventas de Articulos con Tarjeta</strong></td>
-                    <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                    <td width="50%" ><h5>Ventas de Articulos con Tarjeta</h5></td>
+                    <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
                 </tr>
                 <tr>
-                    <td width="50%" ><strong>Ventas de Articulos con Cheque</strong></td>
-                    <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                    <td width="50%" ><h5>Ventas de Articulos con Cheque</h5></td>
+                    <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
                 </tr>
                 <tr>
-                    <td width="50%" ><strong>Ventas de Articulos al Credito</strong></td>
-                    <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                    <td width="50%" ><h5>Ventas de Articulos al Credito</h5></td>
+                    <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
                 </tr>
             </table>
-            <P></P>
             <table  cellspacing="5">
 
                 @foreach($caja as $row)
@@ -32,8 +28,6 @@
                 @endforeach
 
             </table>
-
-        </ul>
     </div>
 </div>
 <div class="panel panel-border panel-custom">
@@ -41,22 +35,22 @@
         <h4 class="panel-title">VENTAS DE PRODUCTOS</h4>
     </div>
     <div class="panel-body">
-        <TABLE>
+        <TABLE class="table table-hover">
         <tr>
-            <td width="50%"><strong>Ventas de Productos al Contado</strong></td>
-            <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="50%"><h5>Ventas de Productos al Contado</h5></td>
+            <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <tr>
-            <td width="50%" ><strong>Ventas de Productos con Tarjeta</strong></td>
-            <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="50%" ><h5>Ventas de Productos con Tarjeta</h5></td>
+            <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <tr>
-            <td width="50%" ><strong>Ventas de Productos con Cheque</strong></td>
-            <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="50%" ><h5>Ventas de Productos con Cheque</h5></td>
+            <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <tr>
-            <td width="50%" ><strong>Ventas de Productos al Credito</strong></td>
-            <td width="15%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="50%" ><h5>Ventas de Productos al Credito</h5></td>
+            <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         </TABLE>
 
@@ -67,14 +61,14 @@
         <h4 class="panel-title">PAGOS DE VENTAS</h4>
     </div>
     <div class="panel-body">
-        <table>
+        <table class="table table-hover">
             <tr>
-                <td width="88.7%" ><strong>Pagos de Articulos</strong></td>
-                <td width="20%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                <td width="50%" ><h5>Pagos de Articulos</h5></td>
+                <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
             </tr>
             <tr>
-                <td width="88.7%" ><strong>Pagos de Productos</strong></td>
-                <td width="20%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+                <td width="50%" ><h5>Pagos de Productos</h5></td>
+                <td width="15%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
             </tr>
         </table>
     </div>
@@ -84,23 +78,23 @@
         <h4 class="panel-title">TOTALES</h4>
     </div>
     <div class="panel-body">
-        <table>
+        <table class="table table-hover">
         <tr>
-            <td width="83%"><strong>Total de Ingresos</strong></td>
-            <td width="20%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="83%"><h5>Total de Ingresos</h5></td>
+            <td width="20%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <tr>
-            <td width="83%"><strong>Total de Gastos</strong></td>
-            <td width="20%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="83%"><h5>Total de Gastos</h5></td>
+            <td width="20%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <tr>
-            <td width="83%"><strong>Total (I - G)</strong></td>
-            <td width="20%" ><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
+            <td width="83%"><h5>Total (I - G)</h5></td>
+            <td width="20%" class="text-right"><strong>{{\App\Tool::convertMoney(0)}}</strong></td>
         </tr>
         <P></P>
         <tr>
-            <td width="83%"><strong>Efectivo en Caja</strong></td>
-            <td width="20%" ><strong>{{\App\Tool::convertMoney(1000)}}</strong></td>
+            <td width="83%"><h5>Efectivo en Caja</h5></td>
+            <td width="20%" class="text-right"><strong>{{\App\Tool::convertMoney(1000)}}</strong></td>
         </tr>
         </table>
     </div>
--- techmark/resources/views/cpanel/admin/caja/partials/infcaja.blade.php
+++ techmark/resources/views/cpanel/admin/caja/partials/infcaja.blade.php
@@ -1,27 +1,39 @@
+<style>
+    h5{
+        margin: 0px 0px 0px 0px;
+    }
+</style>
 <div class="panel panel-border panel-custom">
     <div class="panel-heading">
         <h3 class="panel-title">INFORMACION CAJA</h3>
     </div>
     <div class="panel-body">
-        <ul class="list-unstyled">
-            <P></P>
-            <ul class="list-unstyled">
-                <li><span class="text-custom">Fecha Apertura: </span>{{$caja->registro}}</li>
-                <li><span class="text-custom">Fecha Cierre: </span>{{$caja->fcierre}}</li>
-                <li><span class="text-custom">Usuario Caja: </span>{{$caja->usuario->nombre}}</li>
-                <li><span class="text-custom">Sucursal: </span>{{$caja->sucursal->nombre}}</li>
-                <li><span class="text-custom">Monto de Apertura: </span>{{\App\Tool::convertMoney($caja->apertura)}}</li>
-                <li><span class="text-custom">Monto Cierre: </span>{{\App\Tool::convertMoney($caja->cierre)}}</li>
-            </ul>
-            <table  cellspacing="5">
-
-                @foreach($caja as $row)
-
-                @endforeach
-
-            </table>
-
-        </ul>
+        <table class="table table-hover">
+            <tr>
+                <td><span class="text-custom">Fecha Apertura </span></td>
+                <td>{{$caja->registro}}</td>
+            </tr>
+            <tr>
+                <td><span class="text-custom">Fecha Cierre </span></td>
+                <td>{{$caja->fcierre}}</td>
+            </tr>
+            <tr>
+                <td><span class="text-custom">Usuario Caja</td>
+                <td>{{$caja->usuario->nombre}}</td>
+            </tr>
+            <tr>
+                <td><span class="text-custom">Sucursal </span></td>
+                <td>{{$caja->sucursal->nombre}}</td>
+            </tr>
+            <tr>
+                <td><span class="text-custom">Monto de Apertura </span></td>
+                <td>{{\App\Tool::convertMoney($caja->apertura)}}</td>
+            </tr>
+            <tr>
+                <td><span class="text-custom">Monto Cierre </span></td>
+                <td>{{\App\Tool::convertMoney($caja->cierre)}}</td>
+            </tr>
+        </table>
     </div>
 </div>
 <div class="panel panel-border panel-custom">
