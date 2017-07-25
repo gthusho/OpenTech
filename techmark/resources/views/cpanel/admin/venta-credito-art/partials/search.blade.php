a951f4de1d2e708edcbce363e02139fa99ed2d30
LissC <liseth22794ccl@gmail.com>
2017-07-25 15:20:39 -0400

    correcciones

--- techmark/resources/views/cpanel/admin/venta-credito-art/partials/search.blade.php
+++ techmark/resources/views/cpanel/admin/venta-credito-art/partials/search.blade.php
@@ -9,6 +9,14 @@
         </div><!-- input-group -->
     </div>
     <div class="form-group">
+        <label class="sr-only" for="medida">Sucursal</label>
+        {!! Form::select('sucursal',$sucursales,null,['class'=>'form-control select2','placeholder'=>'Seleccione una Sucursal'])!!}
+    </div>
+    <div class="form-group">
+        <label class="sr-only" for="medida">Proveedor</label>
+        {!! Form::select('cliente',$clientes,null,['class'=>'form-control select2','placeholder'=>'Seleccione un Cliente'])!!}
+    </div>
+    <div class="form-group">
         <label for="name"  class="sr-only">Codigo</label>
         {!! Form::text('s',null,['placeholder'=>'codigo de venta','class'=>'form-control']) !!}
     </div>
