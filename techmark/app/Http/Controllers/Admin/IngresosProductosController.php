277db1f3095df43be4d555324b82c9c2d38a83f7
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-07-26 17:02:59 -0400

    lis

--- techmark/app/Http/Controllers/Admin/IngresosProductosController.php
+++ techmark/app/Http/Controllers/Admin/IngresosProductosController.php
@@ -19,33 +19,45 @@ class IngresosProductosController extends Controller
 {
 
     private  $datos = null;
+    private  $ingreso = null;
+
 
     public function index(Request $request)
     {
 
-        if(Auth::user()->can('allow-read')){
-            $this->datos['brand'] = Tool::brand('Listado de Ingresos ',route('ingresos.articulos.index'),'Ingresos Articulos');
-            $this->datos['ingresos'] = Ingresos::with('articulo','sucursal','compra','almacen')
-                ->compra($request->get('compra'))
-                ->sucursal($request->get('sucursal'))
-                ->articulo($request->get('articulo'))
-                ->orderBy('id','desc')
-                ->paginate();
-            $this->genDatos();
-            return view('cpanel.admin.inarticulos.list')->with($this->datos);
-        }
-
-        \Session::flash('message','No tienes Permiso para visualizar informacion ');
-        return redirect('dashboard');
+//        if(Auth::user()->can('allow-read')){
+//            $this->datos['brand'] = Tool::brand('Listado de Ingresos ',route('ingresos.articulos.index'),'Ingresos Articulos');
+//            $this->datos['ingresos'] = Ingresos::with('articulo','sucursal','compra','almacen')
+//                ->compra($request->get('compra'))
+//                ->sucursal($request->get('sucursal'))
+//                ->articulo($request->get('articulo'))
+//                ->orderBy('id','desc')
+//                ->paginate();
+//            $this->genDatos();
+//            return view('cpanel.admin.inarticulos.list')->with($this->datos);
+//        }
+//
+//        \Session::flash('message','No tienes Permiso para visualizar informacion ');
+//        return redirect('dashboard');
 
     }
-    function genDatos(){
-        $this->datos['sucursales']=Sucursal::where('estado',true)->orderBy('nombre')->pluck('nombre','id');
-        $this->datos['compras']=Compra::pluck('fecha');
-        $this->datos['articulos']=Articulo::orderBy('nombre')->pluck('nombre','id');
-    }
-    
+
     private $produccion =  null;
+    /**
+     * metodo me devuelve o crea una venta
+     */
+    function setIngreso(){
+        //estados = t=>terminado , p=>pendiente
+        $query = Compra::where('usuario_id',Auth::user()->id)->where('estado','p')->get();
+        if(Tool::existe($query)){
+            $this->compra = $query->first();
+        }else{
+            $this->compra = new  Compra();
+            $this->compra->usuario_id = Auth::user()->id;
+            $this->compra->save();
+        }
+    }
+
 
     function create(Request $request){
         $this->produccion = Produccion::find($request->get('idProducction'));
@@ -56,6 +68,7 @@ class IngresosProductosController extends Controller
          * si no es un falso verdadero
          */
         if (!Tool::existe($this->produccion) || $this->produccion->terminado ==1 || $this->produccion->estado=='p' ){
+            $this->setIngreso();
             return redirect()->route('admin.ingresos.productos.index');
         }
         /*
@@ -70,26 +83,5 @@ class IngresosProductosController extends Controller
 
     }
 
-    public function deleteItemsCompra($id)
-    {
-        if(Auth::user()->can('allow-delete')) {
-            $ingreso = Ingresos::find($id);
-            /*
-              * reducimos de stock
-              */
-            $compra = Compra::find($ingreso->compra_id);
-            if($compra->estado =='t'){
 
-                $existencia = new IAManager($ingreso->articulo_id, $ingreso->sucursal_id, $ingreso->almacen_id);
-                $existencia->down($ingreso->cantidad);
-            }
-
-            Ingresos::destroy($id);
-            return redirect()->back();
-            //return redirect()->route('admin.compra.create');
-        }
-        \Session::flash('message','No tienes Permisos para Borrar informacion');
-        return redirect('dashboard');
-
-    }
 }
