4c841e5ac06e4ea68e358128679f6a7f689242b4
Guido Barrientos Paniagua <gthusho@gmail.com>
2017-07-26 20:46:25 -0400

    liss

--- techmark/resources/views/cpanel/admin/ingresosproductos/partials/infoPrecios.blade.php
+++ techmark/resources/views/cpanel/admin/ingresosproductos/partials/infoPrecios.blade.php
@@ -1,28 +1,28 @@
 <fieldset class="scheduler-border">
     <legend class="scheduler-border">DATOS PRECIOS</legend>
     <div class="form-group">
-        {!! Form::label('Unidad ')!!}
-        {!! Form::select('Unidad',[],null,['class'=>'form-control select2','id'=>'atalla'])!!}
+        {!! Form::label('Tallas ')!!}
+        {!! Form::select('tallas',[],null,['class'=>'form-control select2','id'=>'atallas'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('Cantidad ')!!}
         <input name="xCantidad" type="text" class="form-control autonumber cleanclean" autocomplete="off" id="xcantidad" required>
     </div>
-    <div class="form-group">
-        {!! Form::label('Precios (*) ')!!}
-    </div>
-    <div class="form-group">
-        <div class="radio radio-primary radio-inline">
-            <input type="radio" value="1" name="xPrecio" required id="P1" class="cRemove ">
-            <label for="P1"> P1 </label>
-        </div>
-        <div class="radio radio-primary radio-inline">
-            <input type="radio"  value="2" name="xPrecio" required id="P2" class="cRemove ">
-            <label for="P2"> P2 </label>
-        </div>
-        <div class="radio radio-primary radio-inline">
-            <input type="radio"  value="3" name="xPrecio" required id="P3" class="cRemove ">
-            <label for="P3"> P3 </label>
-        </div>
-    </div>
+    {{--<div class="form-group">--}}
+        {{--{!! Form::label('Precios (*) ')!!}--}}
+    {{--</div>--}}
+    {{--<div class="form-group">--}}
+        {{--<div class="radio radio-primary radio-inline">--}}
+            {{--<input type="radio" value="1" name="xPrecio" required id="P1" class="cRemove ">--}}
+            {{--<label for="P1"> P1 </label>--}}
+        {{--</div>--}}
+        {{--<div class="radio radio-primary radio-inline">--}}
+            {{--<input type="radio"  value="2" name="xPrecio" required id="P2" class="cRemove ">--}}
+            {{--<label for="P2"> P2 </label>--}}
+        {{--</div>--}}
+        {{--<div class="radio radio-primary radio-inline">--}}
+            {{--<input type="radio"  value="3" name="xPrecio" required id="P3" class="cRemove ">--}}
+            {{--<label for="P3"> P3 </label>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </fieldset>
