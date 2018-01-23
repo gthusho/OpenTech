c68a7a9fbb900744a25c1fe2e6815f6aa1cb7a7c
Guido Barrientos Paniagua <gthusho@gmail.com>
2018-01-23 17:34:48 -0400

    decimales ventas

--- techmark/resources/views/cpanel/admin/venta_art/addons/js.blade.php
+++ techmark/resources/views/cpanel/admin/venta_art/addons/js.blade.php
@@ -54,10 +54,14 @@
 <script type="text/javascript">
 
     function isNumberKey(evt){
-        var charCode = (evt.which) ? evt.which : event.keyCode
-        if (charCode > 31 && (charCode < 48 || charCode > 57))
-            return false;
-        return true;
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        if(charCode==46)
+            return true;
+         else{
+             if (charCode > 31 && (charCode < 48 || charCode > 57  ))
+                 return false;
+            return true;
+        }
     }
 </script>
 
